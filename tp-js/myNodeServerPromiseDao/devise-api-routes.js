fcb45c9ed967812cc9a55285a522b2c93956c9b3
didierDefrance <didier@d-defrance.fr>
2021-11-12 16:12:27 +0100

    avec axios

--- tp-js/myNodeServerPromiseDao/devise-api-routes.js
+++ tp-js/myNodeServerPromiseDao/devise-api-routes.js
@@ -1,6 +1,8 @@
 var express = require('express');
 const apiRouter = express.Router();
 
+const axios = require('axios'); //pour appeler des WS REST avec des Promises
+
 var deviseDao = require('./devise-dao-mongoose');
 var PersistentdeviseModel = deviseDao.ThisPersistentModel; //to use only for specific extra request (not in dao)
 
@@ -129,5 +131,54 @@ apiRouter.route('/devise-api/public/convert')
 	    res.status(statusCodeFromEx(ex)).send(ex);
     } 
 });
+
+//***********************
+
+
+//http://localhost:8282/devise-api/public/refresh
+apiRouter.route('/devise-api/public/refresh')
+	.get( async function(req , res  , next ) {
+		//URL du web service a appeler:
+		let url = "http://data.fixer.io/api/latest?access_key=26ca93ee7fc19cbe0a423aaa27cab235" //ici avec api-key de didier
+		//type de réponse attendue:
+		/*
+		{"success":true,"timestamp":1635959583,"base":"EUR","date":"2021-11-03",
+		"rates":{"AED":4.254663,"AFN":105.467869,..., "EUR":1 , ...}}
+		*/
+
+		try {
+			const response = await axios.get(url);
+			console.log(response);
+			if(response.status==200){
+				let respValue = response.data;
+				if(respValue.success){
+					//refresh database values:
+					let newRates = respValue.rates;
+					//console.log("newRates="+newRates);
+					for(deviseKey in newRates){
+						let deviseRate = newRates[deviseKey];
+						//console.log(deviseKey + "-" + deviseRate);
+						let devise = { code : deviseKey , change : deviseRate};
+						switch(deviseKey){
+							case "USD" : devise.nom = "Dollar"; break;
+							case "JPY" : devise.nom = "Yen"; break;
+							case "GBP" : devise.nom = "Livre"; break;
+							default : devise = null;
+						}
+						if(devise!=null){
+						   let updateddevise = await deviseDao.updateOne(devise);
+					     }
+					}
+				}
+				res.send(respValue);
+			}else{
+				res.status(500).send({ err : "error - " + error } );
+			}
+		  } catch (error) {
+			console.error(error);
+		  }
+		
+		
+	});
 
 exports.apiRouter = apiRouter;
